15aaa13b787ff948b54339e6c28044284de5bd79
Pedro Gregorio <pedrosgregorio@gmail.com>
2023-08-22 22:02:27 -0300

    Tentando coisas novas

--- monitora/src/login.tsx
+++ monitora/src/login.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 function login() {
   return (
-    <div className='content-under-login'>login</div>
-  )
+    <div className="content-under-login">
+      <div className="card-main">
+        <div>img</div>
+        <div></div>
+      </div>
+    </div>
+  );
 }
 
-export default login
+export default login;
